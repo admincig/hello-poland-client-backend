2f78efa437392f3364df1e36b2b514ca0b5665f5
Dawid Olejnik <dawid@fream.pl>
2018-01-03 15:00:33 +0100

    ear project name

--- ejb/src/main/java/pl/hellopoland/service/FacilityService.java
+++ ejb/src/main/java/pl/hellopoland/service/FacilityService.java
@@ -12,6 +12,7 @@ public class FacilityService extends ServiceSuperclass {
 
   @PermitAll
   public List<Facility> getList() {
-    return em.createQuery("from Facility order by id asc", Facility.class).getResultList();
+    return em.createQuery("from Facility f left join fetch f.mainImage mi order by f.id asc",
+        Facility.class).getResultList();
   }
 }
