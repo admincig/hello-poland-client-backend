86df0367095a6672bc6336b135b69537fc33d18c
Dawid Olejnik <dawid@fream.pl>
2018-02-09 14:26:47 +0100

    test renamed

--- ejb/src/test/java/hellopoland/util/NameAndAddressSplitterTest.java
+++ ejb/src/test/java/hellopoland/util/NameAndAddressSplitterTest.java
@@ -4,15 +4,15 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import org.junit.Test;
 import pl.hellopoland.ConflictingException;
-import pl.hellopoland.util.StringSplit;
+import pl.hellopoland.util.NameAndAddressSplitter;
 
-public class StringSplitTest {
+public class NameAndAddressSplitterTest {
 
   @Test
   public void singleFirstAndLastName() {
     String firstAndLastName = "Jan Kowalski";
-    String firstName = StringSplit.getFirstName(firstAndLastName);
-    String lastName = StringSplit.getLastName(firstAndLastName);
+    String firstName = NameAndAddressSplitter.getFirstName(firstAndLastName);
+    String lastName = NameAndAddressSplitter.getLastName(firstAndLastName);
 
     assertEquals("Jan", firstName);
     assertEquals("Kowalski", lastName);
@@ -21,8 +21,8 @@ public class StringSplitTest {
   @Test
   public void doubleFirstAndLastName() {
     String firstAndLastName = "Jan Maria Kowalski";
-    String firstName = StringSplit.getFirstName(firstAndLastName);
-    String lastName = StringSplit.getLastName(firstAndLastName);
+    String firstName = NameAndAddressSplitter.getFirstName(firstAndLastName);
+    String lastName = NameAndAddressSplitter.getLastName(firstAndLastName);
 
     assertEquals("Jan Maria", firstName);
     assertEquals("Kowalski", lastName);
@@ -31,8 +31,8 @@ public class StringSplitTest {
   @Test
   public void onlyFirstName() {
     String firstAndLastName = "Jan";
-    String firstName = StringSplit.getFirstName(firstAndLastName);
-    String lastName = StringSplit.getLastName(firstAndLastName);
+    String firstName = NameAndAddressSplitter.getFirstName(firstAndLastName);
+    String lastName = NameAndAddressSplitter.getLastName(firstAndLastName);
 
     assertEquals("Jan", firstName);
     assertNull(lastName);
@@ -41,8 +41,8 @@ public class StringSplitTest {
   @Test
   public void singleFirstNameAndDashLastname() {
     String firstAndLastName = "Jan Kowalski-Nowak";
-    String firstName = StringSplit.getFirstName(firstAndLastName);
-    String lastName = StringSplit.getLastName(firstAndLastName);
+    String firstName = NameAndAddressSplitter.getFirstName(firstAndLastName);
+    String lastName = NameAndAddressSplitter.getLastName(firstAndLastName);
 
     assertEquals("Jan", firstName);
     assertEquals("Kowalski-Nowak", lastName);
@@ -51,8 +51,8 @@ public class StringSplitTest {
   @Test
   public void singleFirstNameAndSpaceDashLastname() {
     String firstAndLastName = "Jan Kowalski - Nowak";
-    String firstName = StringSplit.getFirstName(firstAndLastName);
-    String lastName = StringSplit.getLastName(firstAndLastName);
+    String firstName = NameAndAddressSplitter.getFirstName(firstAndLastName);
+    String lastName = NameAndAddressSplitter.getLastName(firstAndLastName);
 
     assertEquals("Jan", firstName);
     assertEquals("Kowalski-Nowak", lastName);
@@ -61,8 +61,8 @@ public class StringSplitTest {
   @Test
   public void doubleFirstNameAndDashLastname() {
     String firstAndLastName = "Jan Maria Kowalski-Nowak";
-    String firstName = StringSplit.getFirstName(firstAndLastName);
-    String lastName = StringSplit.getLastName(firstAndLastName);
+    String firstName = NameAndAddressSplitter.getFirstName(firstAndLastName);
+    String lastName = NameAndAddressSplitter.getLastName(firstAndLastName);
 
     assertEquals("Jan Maria", firstName);
     assertEquals("Kowalski-Nowak", lastName);
@@ -71,8 +71,8 @@ public class StringSplitTest {
   @Test
   public void doubleFirstNameAndSpaceDashLastname() {
     String firstAndLastName = "Jan Maria Kowalski - Nowak";
-    String firstName = StringSplit.getFirstName(firstAndLastName);
-    String lastName = StringSplit.getLastName(firstAndLastName);
+    String firstName = NameAndAddressSplitter.getFirstName(firstAndLastName);
+    String lastName = NameAndAddressSplitter.getLastName(firstAndLastName);
 
     assertEquals("Jan Maria", firstName);
     assertEquals("Kowalski-Nowak", lastName);
@@ -81,8 +81,8 @@ public class StringSplitTest {
   @Test
   public void nullName() {
     String firstAndLastName = null;
-    String firstName = StringSplit.getFirstName(firstAndLastName);
-    String lastName = StringSplit.getLastName(firstAndLastName);
+    String firstName = NameAndAddressSplitter.getFirstName(firstAndLastName);
+    String lastName = NameAndAddressSplitter.getLastName(firstAndLastName);
 
     assertNull(firstName);
     assertNull(lastName);
@@ -91,8 +91,8 @@ public class StringSplitTest {
   @Test
   public void emptyName() {
     String firstAndLastName = "";
-    String firstName = StringSplit.getFirstName(firstAndLastName);
-    String lastName = StringSplit.getLastName(firstAndLastName);
+    String firstName = NameAndAddressSplitter.getFirstName(firstAndLastName);
+    String lastName = NameAndAddressSplitter.getLastName(firstAndLastName);
 
     assertNull(firstName);
     assertNull(lastName);
@@ -101,15 +101,15 @@ public class StringSplitTest {
   @Test(expected = ConflictingException.class)
   public void incorrectName() {
     String firstAndLastName = " )()##!@# - xx";
-    StringSplit.getFirstName(firstAndLastName);
-    StringSplit.getLastName(firstAndLastName);
+    NameAndAddressSplitter.getFirstName(firstAndLastName);
+    NameAndAddressSplitter.getLastName(firstAndLastName);
   }
 
   @Test
   public void singleCityAndCountry() {
     String cityAndCountry = "Wrocław, Poland";
-    String city = StringSplit.getCity(cityAndCountry);
-    String country = StringSplit.getCountry(cityAndCountry);
+    String city = NameAndAddressSplitter.getCity(cityAndCountry);
+    String country = NameAndAddressSplitter.getCountry(cityAndCountry);
 
     assertEquals("Wrocław", city);
     assertEquals("Poland", country);
@@ -118,8 +118,8 @@ public class StringSplitTest {
   @Test
   public void doubleCityAndCountry() {
     String cityAndCountry = "Uraz, Wrocław, Poland";
-    String city = StringSplit.getCity(cityAndCountry);
-    String country = StringSplit.getCountry(cityAndCountry);
+    String city = NameAndAddressSplitter.getCity(cityAndCountry);
+    String country = NameAndAddressSplitter.getCountry(cityAndCountry);
 
     assertEquals("Uraz, Wrocław", city);
     assertEquals("Poland", country);
@@ -128,8 +128,8 @@ public class StringSplitTest {
   @Test
   public void nullCityAndCountry() {
     String cityAndCountry = null;
-    String city = StringSplit.getCity(cityAndCountry);
-    String country = StringSplit.getCountry(cityAndCountry);
+    String city = NameAndAddressSplitter.getCity(cityAndCountry);
+    String country = NameAndAddressSplitter.getCountry(cityAndCountry);
 
     assertNull(city);
     assertNull(country);
@@ -138,8 +138,8 @@ public class StringSplitTest {
   @Test
   public void emptyCityAndCountry() {
     String cityAndCountry = "";
-    String city = StringSplit.getCity(cityAndCountry);
-    String country = StringSplit.getCountry(cityAndCountry);
+    String city = NameAndAddressSplitter.getCity(cityAndCountry);
+    String country = NameAndAddressSplitter.getCountry(cityAndCountry);
 
     assertNull(city);
     assertNull(country);
