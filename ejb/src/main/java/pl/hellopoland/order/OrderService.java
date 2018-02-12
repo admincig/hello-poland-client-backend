53f4d8e517483518a7abe8468e2c0eab7f101197
Dawid Olejnik <dawid@fream.pl>
2018-02-12 09:13:19 +0100

    50% chance for conflict

--- ejb/src/main/java/pl/hellopoland/order/OrderService.java
+++ ejb/src/main/java/pl/hellopoland/order/OrderService.java
@@ -32,7 +32,7 @@ public class OrderService extends ServiceSuperclass {
   @PermitAll
   public Order create(Collection<Triplet<Long, Date, Integer>> triplets, OrderDetails details) {
     // DEVELOPER'S PURPOSES ONLY
-    if (new Random().nextDouble() > 0) {
+    if (new Random().nextDouble() > 0.5) {
       throw new ConflictingException("Brak wolnych biletów na ten dzień");
     }
 
