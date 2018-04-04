07af01db06313b19c05555d0943fb95a23a7e455
Dawid Olejnik <dawid@fream.pl>
2018-04-04 11:04:52 +0200

    new google ids

--- ejb/src/main/java/pl/hellopoland/util/GoogleAPIConnector.java
+++ ejb/src/main/java/pl/hellopoland/util/GoogleAPIConnector.java
@@ -22,7 +22,13 @@ public class GoogleAPIConnector {
                     "876214520825-1um0o7pplgtvifrrju5g21l3aknnckkh.apps.googleusercontent.com",
                     "876214520825-eodmqsked29157pqispetrmabrv6dd8m.apps.googleusercontent.com",
                     "876214520825-jsnb7u85kle6rnafonvitlq9n0ill06p.apps.googleusercontent.com",
-                    "876214520825-9vun30k1bo423jcsfu38ddjcs5jmkfq6.apps.googleusercontent.com"))
+                    "876214520825-9vun30k1bo423jcsfu38ddjcs5jmkfq6.apps.googleusercontent.com",
+                    "319066854009-4r9h35lq6gipp35e4c650ahqcrui3u03.apps.googleusercontent.com",
+                    "319066854009-37dc24k6el5cp5iu9on2ng37j0usi8j7.apps.googleusercontent.com",
+                    "319066854009-hjmsd13qdunboqv3l52mcmq18b6pftmf.apps.googleusercontent.com",
+                    "319066854009-a8v305sfvacpkdqq7t63v9fvcij3r8ts.apps.googleusercontent.com",
+                    "319066854009-vd6lt8aj3373igh6tf9qu78n8krmenme.apps.googleusercontent.com",
+                    "319066854009-shv7nogo8kuqa602kp62jhfe74jdkrv2.apps.googleusercontent.com"))
             .setIssuer("https://accounts.google.com").build();
     logger.info("verifying idToken=" + idToken);
     GoogleIdToken token = verifier.verify(idToken);
