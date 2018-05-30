bef86d219c62ee3b8dff7741de2eca763338980f
Dawid Olejnik <dawid@fream.pl>
2018-05-30 16:18:44 +0200

    partner project first commit

---
+++ partner/src/main/java/hellopoland/partner/RestService.java
@@ -0,0 +1,21 @@
+package hellopoland.partner;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@RequestScoped
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class RestService {
+
+  @GET
+  @Path("/helloWorld")
+  public String helloWorld() {
+    return "Hello, world!";
+  }
+}
