5229780860ad1deddb87d3ac3638209ee768590f
Konrad Bączyński <konrad.baczynski@empirica.pl>
2018-06-20 08:41:41 +0200

    removed useless logs

--- src/main/java/pl/hellopoland/DbFiller.java
+++ src/main/java/pl/hellopoland/DbFiller.java
@@ -3,7 +3,6 @@ package pl.hellopoland;
 import static java.util.Collections.singletonList;
 
 import java.lang.System.Logger;
-import java.lang.System.Logger.Level;
 import javax.annotation.PostConstruct;
 import javax.ejb.DependsOn;
 import javax.ejb.Singleton;
@@ -43,7 +42,6 @@ public class DbFiller extends ServiceSuperclass {
   }
 
   private void createUsers() {
-    logger.log(Level.ERROR, "DbFiller test1");
     User user = new User();
 
     UserRole userRole = new UserRole();
@@ -54,10 +52,7 @@ public class DbFiller extends ServiceSuperclass {
     user.setPassword(passwordEncoder.encode("partner"));
     user.setRoles(singletonList(userRole));
 
-    logger.log(Level.ERROR, "DbFiller test2");
-
     em.persist(user);
-    logger.log(Level.ERROR, "DbFiller test3");
   }
 
 
