f09ab17e1762a6ed2ca22ce6cd4d7f28937028c7
Dawid Olejnik <dawid@fream.pl>
2018-07-30 10:24:54 +0200

    placing orders in api

--- src/main/java/pl/hellopoland/service/OrderService.java
+++ src/main/java/pl/hellopoland/service/OrderService.java
@@ -51,9 +51,9 @@ public class OrderService extends ServiceSuperclass {
     Map<Long, List<Triplet<Long, Date, Integer>>> tripletsGroupedByTicketId =
         triplets.stream().collect(groupingBy(t -> t.first));
     Set<Long> ticketsIds = tripletsGroupedByTicketId.keySet();
-    List<Ticket> tickets =
-        em.createQuery("from Ticket t join fetch t.sight s where t.id in (:ids) order by s.id asc",
-            Ticket.class).setParameter("ids", ticketsIds).getResultList();
+    List<Ticket> tickets = em.createQuery(
+        "from Ticket t join fetch t.sightEvent s where t.id in (:ids) order by s.id asc",
+        Ticket.class).setParameter("ids", ticketsIds).getResultList();
     Map<SightEvent, List<Ticket>> ticketsGroupedBySight =
         tickets.stream().collect(groupingBy(Ticket::getSightEvent));
     Map<Long, Ticket> ticketIdToObject = tickets.stream().collect(toMap(Ticket::getId, t -> t));
