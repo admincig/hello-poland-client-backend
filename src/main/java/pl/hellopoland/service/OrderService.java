69490f2c6996dcfa1e84c6c26c12c52b829168a2
Dawid Olejnik <dawid@fream.pl>
2018-07-31 09:01:11 +0200

    404 when incorrect ticket id

--- src/main/java/pl/hellopoland/service/OrderService.java
+++ src/main/java/pl/hellopoland/service/OrderService.java
@@ -28,6 +28,7 @@ import pl.hellopoland.bo.SightEvent;
 import pl.hellopoland.bo.Ticket;
 import pl.hellopoland.bo.User;
 import pl.hellopoland.exception.conflict.ConflictingException;
+import pl.hellopoland.exception.notfound.ResourceNotFoundException;
 import pl.hellopoland.util.HelloTicket;
 import pl.hellopoland.util.PaymentUtils;
 import pl.hellopoland.util.Triplet;
@@ -54,6 +55,9 @@ public class OrderService extends ServiceSuperclass {
     List<Ticket> tickets = em.createQuery(
         "from Ticket t join fetch t.sightEvent s where t.id in (:ids) order by s.id asc",
         Ticket.class).setParameter("ids", ticketsIds).getResultList();
+    if (tickets.size() < ticketsIds.size()) {
+      throw new ResourceNotFoundException();
+    }
     Map<SightEvent, List<Ticket>> ticketsGroupedBySight =
         tickets.stream().collect(groupingBy(Ticket::getSightEvent));
     Map<Long, Ticket> ticketIdToObject = tickets.stream().collect(toMap(Ticket::getId, t -> t));
