5c9803fc185bd3e5e1302443aac17e17457a198e
Dawid Olejnik <dawid@fream.pl>
2018-08-03 12:06:30 +0200

    using global date time formatter

--- src/main/java/pl/hellopoland/rest/JsonbConfig.java
+++ src/main/java/pl/hellopoland/rest/JsonbConfig.java
@@ -1,5 +1,7 @@
 package pl.hellopoland.rest;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.ext.ContextResolver;
@@ -8,12 +10,14 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class JsonbConfig implements ContextResolver<Jsonb> {
 
+  public static final String DATE_TIME_FORMAT_STRING = "yyyy-MM-dd'T'HH:mm:ssxxx";
+  public static final DateFormat DATE_TIME_FORMAT = new SimpleDateFormat(DATE_TIME_FORMAT_STRING);
+
   private static Jsonb instance;
 
   public static Jsonb getInstance() {
     if (instance == null) {
-      var config =
-          new javax.json.bind.JsonbConfig().withDateFormat("yyyy-MM-dd'T'HH:mm:ssxxx", null);
+      var config = new javax.json.bind.JsonbConfig().withDateFormat(DATE_TIME_FORMAT_STRING, null);
       instance = JsonbBuilder.create(config);
     }
     return instance;
