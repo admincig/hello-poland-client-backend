c8b7b9573fea4a85ed68f514974f5db0a0f525d7
Dawid Olejnik <dawid@fream.pl>
2018-08-13 16:04:53 +0200

    default date format with millis

--- src/main/java/pl/hellopoland/rest/JsonbConfig.java
+++ src/main/java/pl/hellopoland/rest/JsonbConfig.java
@@ -2,6 +2,7 @@ package pl.hellopoland.rest;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.logging.Logger;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.ext.ContextResolver;
@@ -13,7 +14,7 @@ public class JsonbConfig implements ContextResolver<Jsonb> {
   public static final String SIMPLE_DATE_TIME_FORMAT_STRING = "yyyy-MM-dd'T'HH:mm:ssZ";
   public static final DateFormat SIMPLE_DATE_TIME_FORMAT =
       new SimpleDateFormat(SIMPLE_DATE_TIME_FORMAT_STRING);
-  public static final String EXTENDED_DATE_TIME_FORMAT_STRING = "yyyy-MM-dd'T'HH:mm:ssxxx";
+  public static final String EXTENDED_DATE_TIME_FORMAT_STRING = "yyyy-MM-dd'T'HH:mm:ss.SSSxxx";
 
   private static Jsonb instance;
 
@@ -21,6 +22,8 @@ public class JsonbConfig implements ContextResolver<Jsonb> {
     if (instance == null) {
       var config =
           new javax.json.bind.JsonbConfig().withDateFormat(EXTENDED_DATE_TIME_FORMAT_STRING, null);
+      config.setProperty("jsonb.fail-on-unknown-properties", false);
+      Logger.getLogger("JsonbConfig").info(config.getAsMap().toString());
       instance = JsonbBuilder.create(config);
     }
     return instance;
