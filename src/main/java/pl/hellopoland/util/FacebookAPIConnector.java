6dc727881d7d82926f30095ff33a387a9d1aeb23
lmrozek <lmrozek@fream.pl>
2018-08-27 12:11:16 +0200

    test

--- src/main/java/pl/hellopoland/util/FacebookAPIConnector.java
+++ src/main/java/pl/hellopoland/util/FacebookAPIConnector.java
@@ -47,4 +47,9 @@ public class FacebookAPIConnector {
     return Json.createReader(is).readObject();
   }
 
+
+
+  public static void main(String... strings) {
+    FacebookFanPagePostReader.getInstance().readPosts();
+  }
 }
--- src/main/java/pl/hellopoland/util/FacebookFanPagePostReader.java
+++ src/main/java/pl/hellopoland/util/FacebookFanPagePostReader.java
@@ -1,44 +1,81 @@
 package pl.hellopoland.util;
 
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.lang.System.Logger.Level;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import javax.ejb.Singleton;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Properties;
+import javax.json.JsonObject;
+import javax.json.JsonStructure;
+import pl.hellopoland.rest.JsonbConfig;
 
-
-@Singleton
 public class FacebookFanPagePostReader {
-  static final int LIMIT = 100;
-  static final String FANPAGE_ID = "316534255768958";
+  private static final System.Logger logger =
+      System.getLogger(FacebookFanPagePostReader.class.getName());
+  private static final Properties PROPERTIES = System.getProperties();
+  private static final String FANPAGE_ID = PROPERTIES.getProperty("FB.fanpage.id");
+  private static final String PAGE_ACCESS_TOKEN = PROPERTIES.getProperty("FB.page.access.token");
+  private static final int LIMIT = 100;
+  private static final String GRAPH = "https://graph.facebook.com/" + FANPAGE_ID
+      + "/published_posts?access_token=" + PAGE_ACCESS_TOKEN + "&limit=" + LIMIT;
+  private static LocalDateTime timer;
+  private JsonObject posts;
 
-  public static void main(String[] args) {
-    try {
-      var pageAccessToken =
-          "EAADojZAMZB604BAPO42aYwS4k1kZBmZC0cCcz43cnzM9TZA6TQmuRqyDw5j0wFhSzk9QpJM9ZC3UkM7ZAu4z0EWMAt1ysrUflNX1Jr2aYoy46PKJvGwSe8B7rS5ioUf9g1eDUKaJgtufWIch5q1dCHMZBUlKW2oxKUShgUeMper3iOMYrO3wpFHq";
+  private FacebookFanPagePostReader() {}
 
-      var graph = "https://graph.facebook.com/" + FANPAGE_ID + "/published_posts?access_token="
-          + pageAccessToken + "&limit=" + LIMIT;
-      var graphURL = new URL(graph);
-      HttpURLConnection myWebClient = (HttpURLConnection) graphURL.openConnection();
-      String responseMessage = myWebClient.getResponseMessage();
-      if (myWebClient.getResponseCode() != HttpURLConnection.HTTP_OK) {
-        System.out.println(myWebClient.getResponseCode() + " " + responseMessage);
-      } else {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        InputStream is = myWebClient.getInputStream();
-        int r;
-        while ((r = is.read()) != -1) {
-          baos.write(r);
+  private static class SingletonHelper {
+    private static final FacebookFanPagePostReader INSTANCE = new FacebookFanPagePostReader();
+  }
+
+  public static FacebookFanPagePostReader getInstance() {
+    return SingletonHelper.INSTANCE;
+  }
+
+  public void readPosts() {
+    var now = LocalDateTime.now(Clock.tickMinutes(ZoneId.systemDefault()));
+    if (timer == null || timer.until(now, ChronoUnit.MINUTES) > 5) {
+      timer = now;
+
+      try {
+        var graphURL = new URL(GRAPH);
+        HttpURLConnection myWebClient = (HttpURLConnection) graphURL.openConnection();
+        String responseMessage = myWebClient.getResponseMessage();
+        if (myWebClient.getResponseCode() != HttpURLConnection.HTTP_OK) {
+          logger.log(Level.WARNING, "Failed to read posts from funpage id = " + FANPAGE_ID + ". "
+              + myWebClient.getResponseCode() + " " + responseMessage);
+        } else {
+          // ByteArrayOutputStream baos = new ByteArrayOutputStream();
+          InputStream is = myWebClient.getInputStream();
+
+
+          var resp = JsonbConfig.getInstance().fromJson(is, JsonStructure.class);
+          posts = (JsonObject) resp;
+
+          // int r;
+          // while ((r = is.read()) != -1) {
+          // baos.write(r);
+          // }
+          // TODO: to json:
+          // var response = new String(baos.toByteArray());
+          is.close();
+          // baos.close();
+
+
+          System.out.println(posts);
+
+
         }
-        var response = new String(baos.toByteArray());
-        is.close();
-        baos.close();
-        System.out.println(response);
+        myWebClient.disconnect();
+      } catch (Exception e) {
+        logger.log(Level.WARNING, "Failed to read posts from funpage id = " + FANPAGE_ID, e);
       }
-      myWebClient.disconnect();
-    } catch (Exception e) {
-      System.err.println(e);
     }
+
+
+
   }
 }
