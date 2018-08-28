88ec6a9e16734e7fd1ef57029717277daa9b850f
lmrozek <lmrozek@fream.pl>
2018-08-28 11:52:26 +0200

    entpoint address

--- src/main/java/pl/hellopoland/rest/facebook/FacebookRestService.java
+++ src/main/java/pl/hellopoland/rest/facebook/FacebookRestService.java
@@ -8,7 +8,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import pl.hellopoland.util.FacebookFanPagePostReader;
 
-@Path("/fb")
+@Path("/market/fb")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class FacebookRestService {
