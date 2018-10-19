42e1f400f36b130f026432f94fb40cfba327c454
lmrozek <lmrozek@fream.pl>
2018-10-19 12:03:24 +0200

    hpl-487

---
+++ src/main/java/pl/hellopoland/exception/notfound/AccessDeniedException.java
@@ -0,0 +1,13 @@
+package pl.hellopoland.exception.notfound;
+
+import javax.ejb.ApplicationException;
+import javax.ejb.EJBAccessException;
+
+@ApplicationException(rollback = true)
+public class AccessDeniedException extends EJBAccessException {
+  private static final long serialVersionUID = 6190720064110333792L;
+
+  public AccessDeniedException() {
+    super("Access denied to the resource");
+  }
+}
