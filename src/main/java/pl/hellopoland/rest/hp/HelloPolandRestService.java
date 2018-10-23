aaad321ba45bb23790d9a086af65508194e95ea2
lmrozek <lmrozek@fream.pl>
2018-10-23 13:10:47 +0200

    draft

---
+++ src/main/java/pl/hellopoland/rest/hp/HelloPolandRestService.java
@@ -0,0 +1,21 @@
+package pl.hellopoland.rest.hp;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import pl.hellopoland.dto.PartnerDTO;
+
+@RequestScoped
+@Path("/hp")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class HelloPolandRestService {
+
+  @Path("/partner")
+  public PartnerDTO add() {
+
+  }
+
+}
