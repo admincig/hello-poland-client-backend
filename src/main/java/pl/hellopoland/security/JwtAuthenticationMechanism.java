538ccac0a35943a1cba1471bda637b9f8635e0fc
lmrozek <lmrozek@fream.pl>
2018-10-30 16:20:24 +0100

    fixed

--- src/main/java/pl/hellopoland/security/JwtAuthenticationMechanism.java
+++ src/main/java/pl/hellopoland/security/JwtAuthenticationMechanism.java
@@ -134,7 +134,10 @@ public class JwtAuthenticationMechanism implements HttpAuthenticationMechanism {
       validateTokenNotInExpiredTokensList(token);
       tokenProvider.validateToken(token, TokenType.ACCESS_TOKEN);
       JwtCredential credential = tokenProvider.getCredential(token, TokenType.ACCESS_TOKEN);
-
+      if (identityStoreHandler.validate(credential).getStatus()
+          .equals(CredentialValidationResult.NOT_VALIDATED_RESULT.getStatus())) {
+        return context.doNothing();
+      }
       var user = new CurrentUser();
       user.setEmail(credential.getPrincipal());
       user.setRoles(credential.getAuthorities());
