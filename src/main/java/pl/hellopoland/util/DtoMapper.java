9e1d93e3a5eb42a2c0cf320628bccc1170d19e71
lmrozek <lmrozek@fream.pl>
2018-11-07 14:49:38 +0100

    hpl-438 not showing the path to the file

--- src/main/java/pl/hellopoland/util/DtoMapper.java
+++ src/main/java/pl/hellopoland/util/DtoMapper.java
@@ -152,7 +152,7 @@ public class DtoMapper {
   public static FileDescriptorDTO getDTO(FileDescriptor bo) {
     var dto = new FileDescriptorDTO();
     dto.id = bo.getId();
-    dto.path = bo.getPath();
+    // dto.path = bo.getPath();
     dto.created = bo.getCreated();
     dto.type = bo.getMimeType().toString();
     return dto;
