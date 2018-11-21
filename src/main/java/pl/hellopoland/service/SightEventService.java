4abf46c05feceb2e63eb2ea05cc4ab92cfdc8c57
lmrozek <lmrozek@fream.pl>
2018-11-21 17:33:02 +0100

    whole hpl-522

--- src/main/java/pl/hellopoland/service/SightEventService.java
+++ src/main/java/pl/hellopoland/service/SightEventService.java
@@ -31,6 +31,7 @@ import pl.hellopoland.dto.PushDTO;
 import pl.hellopoland.dto.SightEventDTO;
 import pl.hellopoland.dto.TicketDefinitionDTO;
 import pl.hellopoland.dto.TicketPoolDefinitionDTO;
+import pl.hellopoland.exception.conflict.ConflictingException;
 import pl.hellopoland.exception.notfound.AccessDeniedException;
 import pl.hellopoland.rest.dto.AvailableTicketNumberAssociationORO;
 import pl.hellopoland.util.DtoMapper;
@@ -97,6 +98,9 @@ public class SightEventService extends ServiceSuperclass {
   }
 
   public SightEvent create(SightEventDTO dto, Partner partner) {
+    if (dto.sightId == null) {
+      throw new ConflictingException("sightId can't be null.");
+    }
     if (partner == null) {
       partner = partnerService.findByUserEmail(ctx.getCallerPrincipal().getName());
     }
