3f2d6ddb744f9f06a22a59ee379acad282c382e9
lmrozek <lmrozek@fream.pl>
2018-12-13 13:34:45 +0100

    whole task hpl-588

--- src/main/java/pl/hellopoland/service/api/market/SightEventServiceMarketAPI.java
+++ src/main/java/pl/hellopoland/service/api/market/SightEventServiceMarketAPI.java
@@ -40,7 +40,7 @@ public class SightEventServiceMarketAPI {
     }
     List<SightEventDTO> dtos =
         bos.items.stream().map(DtoMapper::getDTO).collect(Collectors.toList());
-    service.fetchTicketPoolDefinitions(bos.items, dtos, false);
+    service.fetchTicketPoolDefinitions(bos.items, dtos, false, true);
     List<SightEventDTO> list = dtos.stream().filter(dto -> service.isAvailable(dto)).map(dto -> {
       dto.ticketPoolDefinitions = null;
       return dto;
@@ -65,7 +65,7 @@ public class SightEventServiceMarketAPI {
         }
       }
       var dto = DtoMapper.getFullDTO(bo);
-      service.fetchTicketPoolDefinitions(List.of(bo), List.of(dto), false);
+      service.fetchTicketPoolDefinitions(List.of(bo), List.of(dto), false, true);
       return service.isAvailable(dto) ? dto : null;
     }
     return null;
