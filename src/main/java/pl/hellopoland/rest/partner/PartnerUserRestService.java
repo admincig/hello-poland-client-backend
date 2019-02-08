ca51846c93e8ef22aa0f9c092fe5ed143fbb7acd
lmrozek <lmrozek@fream.pl>
2019-02-08 10:13:48 +0100

    hpl-622

--- src/main/java/pl/hellopoland/rest/partner/PartnerUserRestService.java
+++ src/main/java/pl/hellopoland/rest/partner/PartnerUserRestService.java
@@ -4,7 +4,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
+import javax.ws.rs.PATCH;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -28,8 +28,8 @@ public class PartnerUserRestService {
     return service.me();
   }
 
-  @PUT
-  @Path("/password")
+  @PATCH
+  @Path("/me/password")
   public Response changePassword(UserAuthDTO user) {
     service.changePassword(user);
     return Response.ok().build();
