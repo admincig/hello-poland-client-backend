0622d3498ddb109a8805aea79c18ae6c87a77623
lmrozek <lmrozek@fream.pl>
2019-02-21 10:02:25 +0100

    hpl-623 1

--- src/main/java/pl/hellopoland/rest/partner/PartnerRestService.java
+++ src/main/java/pl/hellopoland/rest/partner/PartnerRestService.java
@@ -4,9 +4,12 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import pl.hellopoland.dto.UserAuthDTO;
 import pl.hellopoland.rest.dto.PagedCollection;
 import pl.hellopoland.service.api.partner.UserServicePartnerAPI;
 
@@ -23,6 +26,13 @@ public class PartnerRestService {
   @Path("/ushers")
   public PagedCollection getUshers() {
     return service.getUshers();
+  }
+
+  @PATCH
+  @Path("/ushers/{id}/password")
+  public Response changePasswordForUsher(UserAuthDTO usher) {
+    // service.changePassword(usher);
+    return Response.ok().build();
   }
 
 }
