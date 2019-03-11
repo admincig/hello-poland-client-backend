8f0c2e68ef364289d74573f8e6334ba9ebf1c81d
lmrozek <lmrozek@fream.pl>
2019-03-11 18:17:21 +0100

    hpl-698 accept-language parser

--- src/main/java/pl/hellopoland/enums/LanguageVersion.java
+++ src/main/java/pl/hellopoland/enums/LanguageVersion.java
@@ -1,7 +1,10 @@
 package pl.hellopoland.enums;
 
+import java.util.Locale;
+import java.util.Locale.LanguageRange;
+
 public enum LanguageVersion {
-  DE_DE("de-DE"), EN_GB("en-GB"), PL_PL("pl-PL");
+  DE_DE("de-de"), EN_GB("en-gb"), PL_PL("pl-pl");
 
   private String language;
 
@@ -9,44 +12,52 @@ public enum LanguageVersion {
     this.language = language;
   }
 
+  /**
+   * @return language value, e.g. "pl-pl" as "pl-PL"
+   */
+  public String getLanuage() {
+    return language.substring(0, language.length() - 2)
+        .concat(language.substring(language.length() - 2).toUpperCase());
+  }
+
   public static LanguageVersion getForCreateAndUpdateEntity(String language) {
     if (language == null || language.equals("")) {
       return null;
     }
-    for (var lang : LanguageVersion.values()) {
-      if (lang.language.equals(language)) {
-        return lang;
+    var languageRange = Locale.LanguageRange.parse(language);
+    for (LanguageRange range : languageRange) {
+      for (var lang : LanguageVersion.values()) {
+        if (lang.language.equals(range.getRange())) {
+          return lang;
+        }
       }
     }
     return null;
   }
 
-  // TODO:
-  // to correct the situation when the string consists of comma separated language symbols or
-  // statistical weights!!
   public static LanguageVersion getForTranslationEntity(String language) {
     if (language == null || language.equals("")) {
       return null;
     }
-    for (var lang : LanguageVersion.values()) {
-      if (lang.language.equals(language)) {
-        return lang;
+    var languageRange = Locale.LanguageRange.parse(language);
+    for (LanguageRange range : languageRange) {
+      for (var lang : LanguageVersion.values()) {
+        if (lang.language.equals(range.getRange())) {
+          return lang;
+        }
       }
-      switch (language.substring(0, 2)) {
+    }
+    for (LanguageRange range : languageRange) {
+      switch (range.getRange().substring(0, 2)) {
         case "de":
           return LanguageVersion.DE_DE;
         case "en":
           return LanguageVersion.EN_GB;
         case "pl":
           return LanguageVersion.PL_PL;
-        default:
-          return null;
       }
     }
     return null;
   }
 
-  public String getLanuage() {
-    return language;
-  }
 }
