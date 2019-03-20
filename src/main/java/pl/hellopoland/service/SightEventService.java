a444b75aa96757d65886616db3418138158666ba
lmrozek <lmrozek@fream.pl>
2019-03-20 14:24:38 +0100

    hpl-629

--- src/main/java/pl/hellopoland/service/SightEventService.java
+++ src/main/java/pl/hellopoland/service/SightEventService.java
@@ -491,10 +491,12 @@ public class SightEventService extends ServiceSuperclass {
   public void setSightEventPromotion(Long id, Integer promotion) {
     var bo = Optional.ofNullable(get(id))
         .orElseThrow(() -> new ConflictingException("Resource not found"));
-    bo.setPromotion(promotion);
     em.createQuery("from SightEvent where promotion = :promotion", SightEvent.class)
-        .setParameter("promotion", promotion).getResultStream()
-        .forEach(se -> se.setPromotion(null));
+        .setParameter("promotion", promotion).getResultList().forEach(se -> {
+          se.setPromotion(null);
+          em.flush();
+        });
+    bo.setPromotion(promotion);
   }
 
 }
