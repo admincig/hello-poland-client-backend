79d9ba46208e82a646048603efdac8a87d207000
lmrozek <lmrozek@fream.pl>
2019-07-08 12:11:50 +0200

    hpl-807

--- src/main/java/pl/hellopoland/service/OrderService.java
+++ src/main/java/pl/hellopoland/service/OrderService.java
@@ -8,10 +8,12 @@ import java.lang.System.Logger.Level;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -58,6 +60,27 @@ public class OrderService extends ServiceSuperclass {
   AgreementService aService;
 
   public PassageCart create(OrderIRO iro) {
+    var expired = new HashMap<Long, OrderEntryIRO>();
+    iro.entries.forEach(entry -> {
+      if (entry.date.before(new Date())) {
+        expired.put(entry.id, entry);
+      }
+    });
+    if (expired.size() > 0) {
+      List<TicketDefinition> tickets = em.createQuery(
+          "from TicketDefinition t join fetch t.sightEvent s where t.id in (:ids) order by s.id asc",
+          TicketDefinition.class).setParameter("ids", expired.keySet()).getResultList();
+
+      var format = new SimpleDateFormat("YYYY-MM-dd HH:mm");
+      var errMsg = new StringBuilder(
+          "W swoim koszyku masz bilety na oferty, które już minęły. Przeterminowane bilety:");
+
+      tickets.forEach(t -> errMsg
+          .append(" " + t.getName() + ", data: " + format.format(expired.get(t.getId()).date)
+              + ", oferta: " + t.getSightEvent().getName() + ";"));
+
+      throw new ConflictingException(errMsg.toString());
+    }
     Order o = new Order();
     o.generateHash();
     o.setUser(getLoggedUser());
