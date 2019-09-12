98d796c82955051551783338cb971ef8767c1980
Dawid Olejnik <dawid@fream.pl>
2019-09-12 14:51:01 +0200

    próba naprawienia problemów w komunikacji hpl-hpt

--- src/main/java/pl/hellopoland/service/SightEventService.java
+++ src/main/java/pl/hellopoland/service/SightEventService.java
@@ -564,7 +564,7 @@ public class SightEventService extends ServiceSuperclass {
     bo = BeanUtils.copyNotNullProperties(translation, bo);
     em.merge(bo);
     if (bo.getPortal().getType() == Portal.Type.HELLOTICKET_CLOUD_1) {
-      Partner partner = partnerService.findByUserEmail(ctx.getCallerPrincipal().getName());
+      Partner partner = bo.getPartner();
       Portal hpt = getPortal("Hello Ticket Cloud");
       HelloTicket helloTicket = new HelloTicket(hpt.getUrl());
       helloTicket.updateSightEvent(DtoMapper.getDTO(bo), partner.getHptToken());
