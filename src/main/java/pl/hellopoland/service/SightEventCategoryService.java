523dac111d188bd6ec62ed893dfc139387e47482
Dawid Olejnik <dawid@fream.pl>
2019-09-12 20:27:42 +0200

    flush

--- src/main/java/pl/hellopoland/service/SightEventCategoryService.java
+++ src/main/java/pl/hellopoland/service/SightEventCategoryService.java
@@ -5,15 +5,17 @@ import pl.hellopoland.bo.Category;
 import pl.hellopoland.bo.SightEvent;
 
 @Stateless
-public class SightEventCategoryService {
+public class SightEventCategoryService extends ServiceSuperclass {
 
   public SightEvent addCategory(SightEvent se, Category cat) {
     se.getCategories().add(cat);
+    em.flush();
     return se;
   }
 
   public SightEvent removeCategory(SightEvent se, Category cat) {
     se.getCategories().remove(cat);
+    em.flush();
     return se;
   }
 }
