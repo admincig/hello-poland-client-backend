de77f3f4fd06fa1c7848ffbb45b59e762e3ce03d
Dawid Olejnik <dawid@fream.pl>
2019-09-25 16:06:52 +0200

    market api do pobierania partnerów

--- src/main/java/pl/hellopoland/rest/market/MarketPartnerRestService.java
+++ src/main/java/pl/hellopoland/rest/market/MarketPartnerRestService.java
@@ -4,9 +4,13 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import pl.hellopoland.dto.MarketPartnerDTO;
+import pl.hellopoland.rest.RestService;
 import pl.hellopoland.rest.dto.PagedCollection;
 import pl.hellopoland.service.api.market.PartnerServiceMarketAPI;
 
@@ -20,8 +24,16 @@ public class MarketPartnerRestService {
   PartnerServiceMarketAPI service;
 
   @GET
-  public PagedCollection list() {
-    return service.list();
+  public PagedCollection list(
+      @HeaderParam("Content-Language") String contentLanguage) {
+    return service.list(RestService.parseLang(contentLanguage));
+  }
+
+  @GET
+  @Path("/{id}")
+  public MarketPartnerDTO getPartner(@PathParam("id") Long id,
+      @HeaderParam("Content-Language") String contentLanguage) {
+    return service.get(id, RestService.parseLang(contentLanguage));
   }
 
 }
--- src/main/java/pl/hellopoland/service/api/market/PartnerServiceMarketAPI.java
+++ src/main/java/pl/hellopoland/service/api/market/PartnerServiceMarketAPI.java
@@ -8,8 +8,10 @@ import javax.inject.Inject;
 import pl.hellopoland.bo.Partner;
 import pl.hellopoland.config.PartnerPagedCollectionConfig;
 import pl.hellopoland.dto.MarketPartnerDTO;
+import pl.hellopoland.enums.LanguageVersion;
 import pl.hellopoland.rest.dto.PagedCollection;
 import pl.hellopoland.service.PartnerService;
+import pl.hellopoland.service.TranslationService;
 import pl.hellopoland.util.DtoMapper;
 import pl.hellopoland.util.PagedEntityCollection;
 
@@ -18,13 +20,23 @@ public class PartnerServiceMarketAPI {
 
   @Inject
   PartnerService service;
+  @Inject
+  TranslationService transService;
 
   @PermitAll
-  public PagedCollection list() {
+  public PagedCollection list(LanguageVersion languageVersion) {
     PartnerPagedCollectionConfig config = new PartnerPagedCollectionConfig();
     PagedEntityCollection<Partner> pec = service.getList(config);
+    pec.items = transService.translateEntities(pec.items, languageVersion, false);
     List<MarketPartnerDTO> dtos =
         pec.items.stream().map(DtoMapper::getMarketDTO).collect(Collectors.toList());
     return new PagedCollection(dtos, config);
+  }
+
+  @PermitAll
+  public MarketPartnerDTO get(Long id, LanguageVersion parseLang) {
+    Partner bo = service.getPartnerWithCategoriesAndCities(id);
+    bo = transService.translateEntity(bo, parseLang, true);
+    return DtoMapper.getFullMarketPartnerDTO(bo);
   }
 }
