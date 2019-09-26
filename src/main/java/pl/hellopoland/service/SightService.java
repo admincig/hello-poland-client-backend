19a2b8d42c8019d0830446ce189981b92b6e11a7
Dawid Olejnik <dawid@fream.pl>
2019-09-26 09:40:42 +0200

    sighty nie mają kategorii

--- src/main/java/pl/hellopoland/service/SightService.java
+++ src/main/java/pl/hellopoland/service/SightService.java
@@ -125,7 +125,6 @@ public class SightService extends ServiceSuperclass {
     }
     bo.getAvailableLanguageVersions().size();
     bo.getAgreements().size();
-    bo.getCategories().size();
     bo.getImages().size();
     bo.getOpeningHours().size();
     return bo;
