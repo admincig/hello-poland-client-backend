d0bba078b8e9052d3fee468f6ead1482535d25e7
Dawid Olejnik <dawid@fream.pl>
2019-09-30 21:15:59 +0200

    partner tags

---
+++ src/main/java/pl/hellopoland/service/api/partner/TagServicePartnerAPI.java
@@ -0,0 +1,41 @@
+package pl.hellopoland.service.api.partner;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import pl.hellopoland.bo.Tag;
+import pl.hellopoland.config.TagPagedCollectionConfig;
+import pl.hellopoland.dto.TagDTO;
+import pl.hellopoland.enums.LanguageVersion;
+import pl.hellopoland.rest.dto.PagedCollection;
+import pl.hellopoland.service.TagService;
+import pl.hellopoland.service.TranslationService;
+import pl.hellopoland.util.DtoMapper;
+
+@Stateless
+public class TagServicePartnerAPI {
+
+  @Inject
+  TagService service;
+  @Inject
+  TranslationService tService;
+
+  @RolesAllowed("partner")
+  public PagedCollection pagedList(LanguageVersion language) {
+    var config = new TagPagedCollectionConfig();
+    var bos = service.pagedList(config);
+    bos.items = tService.translateEntities(bos.items, language, false);
+    List<TagDTO> dtos = bos.items.stream().map(DtoMapper::getDTO).collect(Collectors.toList());
+    return new PagedCollection(dtos, bos.config);
+  }
+
+  @RolesAllowed("partner")
+  public TagDTO get(Long id, LanguageVersion language) {
+    Tag tag = service.get(id);
+    tag = tService.translateEntity(tag, language, true);
+    return DtoMapper.getFullDTO(tag);
+  }
+
+}
