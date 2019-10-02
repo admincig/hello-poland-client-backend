6a11801eba3456d90c9a26aa361cdb53006480a6
wiktor <wwolakksiazek@fream.pl>
2019-10-02 15:16:57 +0200

    returning id and name for every sight in market api

--- src/main/java/pl/hellopoland/config/SightPagedCollectionConfig.java
+++ src/main/java/pl/hellopoland/config/SightPagedCollectionConfig.java
@@ -9,7 +9,7 @@ public class SightPagedCollectionConfig extends PagedCollectionConfig<Sight> {
 
   @Override
   public String joins() {
-    return "left join fetch e.mainImage mi";// join fetch e.sightEvents ses";
+    return "left join fetch e.mainImage mi join fetch e.partner p";// join fetch e.sightEvents ses";
   }
 
   public void setSearchQuery(String searchQuery) {
