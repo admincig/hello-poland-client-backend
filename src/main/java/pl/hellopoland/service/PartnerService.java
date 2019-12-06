3ae7a16d27da0bbaf919750a9d5dfd3beefbd476
Dawid Olejnik <dawid@fream.pl>
2019-12-06 15:36:13 +0100

    zmiana domyslnego jezyka partnera fix z adresem

--- src/main/java/pl/hellopoland/service/PartnerService.java
+++ src/main/java/pl/hellopoland/service/PartnerService.java
@@ -84,12 +84,14 @@ public class PartnerService extends ServiceSuperclass {
   }
 
   public Partner changeDefaultLanguage(Partner bo, LanguageVersion lang) {
+    if (bo.getAddress() != null) {
+      Address aTranslation = translationService.translateEntity(bo.getAddress(), lang);
+      bo.getAddress().setDefaultLanguage(lang);
+      bo.getAddress().setDirections(aTranslation.getDirections());
+    }
     Partner translation = translationService.translateEntity(bo, lang);
-    Address aTranslation = translationService.translateEntity(bo.getAddress(), lang);
     bo = BeanUtils.copyNotNullProperties(translation, bo);
     bo.setDefaultLanguage(lang);
-    bo.getAddress().setDefaultLanguage(lang);
-    bo.getAddress().setDirections(aTranslation.getDirections());
     em.merge(bo);
     em.flush();
     return bo;
