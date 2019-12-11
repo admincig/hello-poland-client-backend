9a921ae5776e53456105615bf48ea7e87f98409d
Dawid Olejnik <dawid@fream.pl>
2019-12-11 10:26:53 +0100

    200 zamiast 409 po podwójnym zapisaniu do listy

--- src/main/java/pl/hellopoland/service/api/market/MailingListServiceMarketAPI.java
+++ src/main/java/pl/hellopoland/service/api/market/MailingListServiceMarketAPI.java
@@ -1,21 +1,31 @@
 package pl.hellopoland.service.api.market;
 
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
 import javax.annotation.security.PermitAll;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import pl.hellopoland.exception.conflict.ConflictingException;
 import pl.hellopoland.service.MailingListService;
 
 @Stateless
 public class MailingListServiceMarketAPI {
 
   @Inject
+  Logger logger;
+  @Inject
   MailingListService service;
 
   @PermitAll
   public String addToMailingList(String email) {
     service.getOrCreateContact(email);
-    String contactAddedToList = service.addContactToList(email);
-    service.sendConfirmationMail(email);
-    return contactAddedToList;
+    try {
+      service.addContactToList(email);
+      service.sendConfirmationMail(email);
+    } catch (ConflictingException e) {
+      logger.log(Level.WARNING,
+          "email " + email + " has not been added to mailing list: " + e.getMessage());
+    }
+    return email;
   }
 }
