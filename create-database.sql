b12073daaeb690a0388070d2ff73de6112a1d408
Dawid Olejnik <dawid@fream.pl>
2021-01-12 14:01:27 +0100

    no more compose, big change in CI variables

--- create-database.sql
+++
@@ -1,2 +0,0 @@
-create user hellopoland with password 'hellopoland';
-create database hellopoland owner hellopoland ENCODING = 'UTF8' TABLESPACE = pg_default LC_COLLATE = 'en_US.UTF-8' CONNECTION LIMIT = -1 TEMPLATE template0;
